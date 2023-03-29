9bf6b858662e3d2917642565a5bb290be98685c9
Udit Keskar <120160155+udit2311@users.noreply.github.com>
2023-03-29 12:38:42 +0530

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,8 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
     println("HIIIIIIII")
-
+    println("Program arguments: ${args.joinToString()}")
+    println("HIIIIIIII")
 
     //asdasdadasdad
 
