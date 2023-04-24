47f070a099153c864b3628f4fa99bcbb68e1f047
Udit Keskar <udit.keskar@springernature.com>
2023-04-24 16:55:45 +0530

    Scope Functions: [let] done

--- src/main/kotlin/conceptPractice/scopeFunctions/Let.kt
+++ src/main/kotlin/conceptPractice/scopeFunctions/Let.kt
@@ -1,5 +1,20 @@
 package conceptPractice.scopeFunctions
 
-fun main(args: Array<String>) {
+import java.util.*
+import kotlin.reflect.typeOf
 
+fun main(args: Array<String>) {
+    val name: String? = null
+//  println(name!!)  // gives NullPointerException
+
+    name?.let {
+        println(it)      // won't execute as [name] is [null]
+    }
+
+    val sc = Scanner(System.`in`)
+    print("Kindly type anything: ")
+    val anotherName: Any? = sc.nextLine()
+    anotherName?.let {
+        println(it)     // prints value of [anotherName] as it's [not null]
+    }
 }
