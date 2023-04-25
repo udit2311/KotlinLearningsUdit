96c2daa0dc974a5139d7441ed3685af7a42c7ab8
Udit Keskar <udit.keskar@springernature.com>
2023-04-25 17:03:55 +0530

    Scope Functions: [with] completed

--- src/main/kotlin/conceptPractice/scopeFunctions/With.kt
+++ src/main/kotlin/conceptPractice/scopeFunctions/With.kt
@@ -3,21 +3,21 @@ package conceptPractice.scopeFunctions
 // allows you to run code on an object without calling any of its methods
 
 class Company{
-    var a: String = ""
-    var b: Int = 0
+    var a: String = "a"
+    var b: Int = 7
 }
 
 fun main(args: Array<String>) {
     val companyObj: Company = Company()
+    val a = 12
+    val b = "b"
 
-    println(with(companyObj){
-        this.a = "String is assigned to 'a'"
-        this.b = 7
-    })
-
-    val print = with(companyObj){
-        this.a = "String is assigned to 'a'"
-        this.b = 7
+    with(companyObj){
+        println(this.a)
+        println(this.b)
     }
+
+    println(a)
+    println(b)
 
 }
